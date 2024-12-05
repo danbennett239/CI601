69941953f1218cac30e4e6a53474224beb442b64
Dan Bennett <danieljbennett@ymail.com>
2024-12-05 19:18:10 +0000

    Add Hasura and Docker

---
+++ hasura/migrations/default/1733426094153_create_users_table/down.sql
@@ -0,0 +1,10 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TABLE users (
+--     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     name TEXT NOT NULL,
+--     email TEXT UNIQUE NOT NULL,
+--     password TEXT NOT NULL,
+--     created_at TIMESTAMP DEFAULT now(),
+--     updated_at TIMESTAMP DEFAULT now()
+-- );
