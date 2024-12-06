284ac7be46b6170a9bbfd3e6173e7aabe0a6653a
Dan Bennett <danieljbennett@ymail.com>
2024-12-06 14:32:09 +0000

    Add sign in and register

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Banner />
         {children}
       </body>
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import styles from "./Banner.module.css";
 
 const Banner: React.FC = () => {
   return (
     <div className={styles.banner}>
+      {/* Logo Section */}
       <div className={styles.logoSection}>
         <img
           src="/logo.png" // Replace with the actual path to your logo
@@ -14,6 +16,13 @@ const Banner: React.FC = () => {
         />
         <span className={styles.name}>Tempname</span>
       </div>
+
+      {/* Sign In Section */}
+      <Link href="/signin">
+        <div className={styles.signInSection}>
+          Sign In
+        </div>
+      </Link>
     </div>
   );
 };
