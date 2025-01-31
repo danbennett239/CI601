ffb4cebf112bbb93721b4703407a83c1639f87d4
Dan Bennett <danieljbennett@ymail.com>
2025-01-31 21:50:37 +0000

    Update practice route

--- src/components/PracticeRegisterForm/PracticeRegisterForm.tsx
+++ src/components/PracticeRegisterForm/PracticeRegisterForm.tsx
@@ -198,7 +198,7 @@ export default function PracticeRegisterForm({ onSuccess }: PracticeRegisterForm
     console.log("Practice Register Form Data:", formData);
 
     try {
-      const res = await fetch("/api/practices/register", {
+      const res = await fetch("/api/practice/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
