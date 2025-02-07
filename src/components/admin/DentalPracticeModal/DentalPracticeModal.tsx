ffb2a21221128b6320d159fd9d134e4bf4691ce5
Dan Bennett <danieljbennett@ymail.com>
2025-02-07 15:52:50 +0000

    Add Address Type

--- src/components/admin/DentalPracticeModal/DentalPracticeModal.tsx
+++ src/components/admin/DentalPracticeModal/DentalPracticeModal.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { approvePractice, denyPractice } from "@/lib/services/practiceService";
 import styles from "./DentalPracticeModal.module.css";
 import { Practice } from "@/types/practice";
 
--- src/types/practice.ts
+++ src/types/practice.ts
@@ -4,10 +4,19 @@ export interface Practice {
   email: string;
   phone_number: string;
   photo?: string;
-  address: Record<string, any>;
+  address: Address
   opening_hours: Array<{ dayName: string; open: string; close: string }>;
   verified: boolean;
   created_at?: string;
   updated_at?: string;
   verified_at?: string;
+}
+export interface Address {
+  line1: string;
+  line2?: string;
+  line3?: string;
+  city: string;
+  county?: string;
+  postcode: string;
+  country: string;
 }
