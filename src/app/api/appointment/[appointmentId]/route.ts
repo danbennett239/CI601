305d08c18986b5d6f727dac18a780e55df8823f5
Dan Bennett <danieljbennett@ymail.com>
2025-02-14 18:10:37 +0000

    Appointment route interface

--- src/app/api/appointment/[appointmentId]/route.ts
+++ src/app/api/appointment/[appointmentId]/route.ts
@@ -1,11 +1,14 @@
-// app/api/appointments/[appointmentId/route.ts
-
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getAppointmentById, updateAppointment, deleteAppointment } from '@/lib/services/appointmentService';
 
-export async function GET(request: Request, { params }: { params: { appointmentId: string } }) {
+// Define a type for route parameters
+interface Params {
+  appointmentId: string;
+}
+
+export async function GET(request: NextRequest, context: { params: Params }) {
   try {
-    const appointment = await getAppointmentById(params.appointmentId);
+    const appointment = await getAppointmentById(context.params.appointmentId);
     return NextResponse.json({ appointment });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Failed to fetch appointment.';
@@ -13,13 +16,14 @@ export async function GET(request: Request, { params }: { params: { appointmentI
   }
 }
 
-export async function PUT(request: Request, { params }: { params: { appointmentId: string } }) {
+export async function PUT(request: NextRequest, context: { params: Params }) {
   try {
     const body = await request.json();
     if (!body) {
       return NextResponse.json({ error: "Request body is missing" }, { status: 400 });
     }
-    const appointment = await updateAppointment(params.appointmentId, body);
+
+    const appointment = await updateAppointment(context.params.appointmentId, body);
     return NextResponse.json({ appointment });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Failed to update appointment.';
@@ -27,9 +31,9 @@ export async function PUT(request: Request, { params }: { params: { appointmentI
   }
 }
 
-export async function DELETE(request: Request, { params }: { params: { appointmentId: string } }) {
+export async function DELETE(request: NextRequest, context: { params: Params }) {
   try {
-    await deleteAppointment(params.appointmentId);
+    await deleteAppointment(context.params.appointmentId);
     return NextResponse.json({ success: true });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Failed to delete appointment.';
