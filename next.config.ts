ad2f29f8d65239d61ed8bcac20759ba23ad58721
Dan Bennett <danieljbennett@ymail.com>
2025-03-05 16:55:46 +0000

    Add S3 to Next Image Config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["ci601-dental-practice-images.s3.eu-north-1.amazonaws.com"],
+  },
   async redirects() {
     return [
       {
